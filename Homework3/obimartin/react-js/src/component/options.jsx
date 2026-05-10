9d7c1b6a9fa6f4a40990516728566b1c724ac9b8
Robin Martin <robinkjbird@gmail.com>
2026-05-09 23:24:06 -0700

    final

--- Homework3/obimartin/react-js/src/component/options.jsx
+++ Homework3/obimartin/react-js/src/component/options.jsx
@@ -1,10 +1,10 @@
-import Data from "../../data/demo.json";
+const csvFiles = import.meta.glob("../../data/stockdata/*.csv");
+const stocks = Object.keys(csvFiles).map(path => path.split("/").pop().replace(".csv", ""));
 
 export default function RenderOptions() {
-    const bars = Data.data;
-    return bars.map((bar, index) => (
-      <option key={index} value={bar.category}>
-        {bar.category}
+    return stocks.map((ticker, index) => (
+      <option key={index} value={ticker}>
+        {ticker}
       </option>
     ));
   }
